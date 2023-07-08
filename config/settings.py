2b28e1660b42cd417907316816c39a03bc3c1583
Haradhan Sharma <haradhan.sharma@gmail.com>
2023-07-08 19:48:44 +0600

    apps setup

--- config/settings.py
+++ config/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'urlshort',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',  # Or the IP address of your MySQL server
+        'PORT': '3306',  # Or the port number used by your MySQL server
     }
 }
 
